990d8fc93667fa18730269988fbcc792924a10d5
makemesuffer <strigaliov4working@gmail.com>
2019-03-24 19:37:06 +0300

    super

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  str = str.split('');
+  let strLength = str.length;
+  while(strLength>1) {
+      for (let i = 0; i < str.length; i++) {
+          for (let j = 0; j < bracketsConfig.length; j++) {
+              if (str[i] === bracketsConfig[j][0] && str[i + 1] === bracketsConfig[j][1]) str.splice(i, 2);
+          }
+      }
+      strLength -=2;
+      if(str.length === 0) return true;
+  }
+
+      return false;
+};
